b7b337e93e64d00ac783b6e52ed4e743a390b291
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-09-07 18:56:28 +0530

    error

--- Utils/ApiError.js
+++ Utils/ApiError.js
@@ -1,10 +1,11 @@
 class ApiError extends Error {
-  constructor (message, statusCode) {
+  constructor (message, statusCode, name) {
     super(message);
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, this.constructor);
     }
-    this.statusCode = statusCode;
+    this.statusCode = statusCode || 400;
+    this.name = name || this.constructor.name;
   }
 }
 global.ApiError = ApiError;
--- Utils/error.js
+++ Utils/error.js
@@ -1,7 +1,7 @@
 const getError = (e, res) => {
   if (res.headersSent) return;
-  console.error(e)
-  return res.status(500).json({error: e.message.replace('Error: ', ''),type:e.name});
+  console.error(e);
+  return res.status(e.statusCode || 500).json({ error: e.message.replace('Error: ', ''), type: e.name });
 };
 
 export default getError;
