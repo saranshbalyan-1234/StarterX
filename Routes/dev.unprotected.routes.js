de04c534f2ec99fe88213fa6c15725a356394393
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-09-09 18:06:55 +0530

    swagger removed

--- Routes/dev.unprotected.routes.js
+++ Routes/dev.unprotected.routes.js
@@ -1,11 +1,4 @@
 import express from 'express';
-// Swagger
-import { createRequire } from 'module';
-import passport from 'passport';
-import swaggerUi from 'swagger-ui-express';
-const require = createRequire(import.meta.url);
-const swaggerFile = require('../Utils/Swagger/swagger.json');
-// Swagger
 
 const Router = express.Router();
 
@@ -13,6 +6,6 @@ Router.get('/health', (_req, res) =>
   res.json('Server is Working')
 );
 
-Router.use('/swagger', passport.authenticate('basic', { session: false }), swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 
 export default Router;
