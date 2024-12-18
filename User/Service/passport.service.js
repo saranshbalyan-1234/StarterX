6b394e98489521912f90cef21c5b51ad81879cc7
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-12-19 01:54:05 +0530

    test

--- User/Service/passport.service.js
+++ User/Service/passport.service.js
@@ -1,88 +1,9 @@
 import passport from 'passport';
-import { Strategy as GitHubStrategy } from 'passport-github2';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { Strategy as MicrosoftStrategy } from 'passport-microsoft';
 import OpenIDConnectStrategy from 'passport-openidconnect';
 import { Strategy as SamlStrategy } from 'passport-saml';
 
 import { loginWithCredentals } from './user.service.js';
 
-/**
- * Sign in with Google.
- */
-if (process.env.GOOGLE_ID && process.env.GOOGLE_SECRET) {
-  const googleStrategyConfig = new GoogleStrategy(
-    {
-      callbackURL: '/passport/google/callback',
-      clientID: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      passReqToCallback: true
-    },
-    async (req, _accessToken, _refreshToken, profile, done) => {
-      try {
-        const email = profile.emails[0]?.value;
-        // eslint-disable-next-line sonarjs/no-duplicate-string
-        const loggedInUser = await loginWithCredentals({ email, isPassRequired: false, rememberMe: true, tenant: req.headers['x-tenant-id'] });
-        return done(null, loggedInUser);
-      } catch (err) {
-        return done(err);
-      }
-    }
-  );
-  passport.use('google', googleStrategyConfig);
-}
-
-/**
- * Sign in with Microsoft.
- */
-if (process.env.MICROSOFT_ID && process.env.MICROSOFT_SECRET) {
-  const microsoftStrategyConfig = new MicrosoftStrategy(
-    {
-      callbackURL: '/auth/microsoft/callback',
-      clientID: process.env.MICROSOFT_ID,
-      clientSecret: process.env.MICROSOFT_SECRET,
-      passReqToCallback: true,
-      scope: ['user.read']
-    },
-    async (req, _accessToken, _refreshToken, profile, done) => {
-      try {
-        const email = profile.emails[0]?.value;
-        // eslint-disable-next-line sonarjs/no-duplicate-string
-        const loggedInUser = await loginWithCredentals({ email, isPassRequired: false, rememberMe: true, tenant: req.headers['x-tenant-id'] });
-        return done(null, loggedInUser);
-      } catch (err) {
-        return done(err);
-      }
-    }
-  );
-  passport.use('microsoft', microsoftStrategyConfig);
-}
-
-/**
- * Sign in with GitHub.
- */
-if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
-  const githubStrategyConfig = new GitHubStrategy(
-    {
-      callbackURL: '/auth/github/callback',
-      clientID: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      passReqToCallback: true,
-      scope: ['user:email']
-    },
-    async (req, _accessToken, _refreshToken, profile, done) => {
-      try {
-        const email = profile.emails[0]?.value;
-        // eslint-disable-next-line sonarjs/no-duplicate-string
-        const loggedInUser = await loginWithCredentals({ email, isPassRequired: false, rememberMe: true, tenant: req.headers['x-tenant-id'] });
-        return done(null, loggedInUser);
-      } catch (err) {
-        return done(err);
-      }
-    }
-  );
-  passport.use('github', githubStrategyConfig);
-}
 
 passport.use(new SamlStrategy(
   {
