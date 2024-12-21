434dc4ea3798494b42d0db8b5424ce64f45af291
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-12-21 20:37:37 +0530

    remove extra routed

--- User/Routes/auth.unprotected.routes.js
+++ User/Routes/auth.unprotected.routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { validate } from 'express-validation';
-import passport from 'passport';
 
 import { login, register, resendVerificationMail, resetPassword, sendResetPasswordMail, verifyCustomer } from '../Controllers/auth.controller.js';
 import { emailBodyValidation, loginValidation, registerValidation, tokenParamsValidation } from '../Validations/auth.js';
@@ -12,20 +11,6 @@ Router.get('/verify/:token', validate(tokenParamsValidation), verifyCustomer);
 Router.post('/verify/send-mail', validate(emailBodyValidation), resendVerificationMail);
 Router.post('/reset-password/send-mail', validate(emailBodyValidation), sendResetPasswordMail);
 Router.post('/reset-password/:token', validate(tokenParamsValidation), resetPassword);
-
-Router.get('/saml', passport.authenticate('saml'));
-Router.post('/saml/callback', passport.authenticate('saml', { failureRedirect: '/auth/saml', session: false }), (req, res) => {
-  console.log('saransh', req);
-  const { accessToken, refreshToken } = req.user;
-  return res.redirect(`${process.env.WEBSITE_HOME}/login?accessToken=${accessToken}&refreshToken=${refreshToken}`);
-});
-
-Router.get('/oidc', passport.authenticate('openidconnect'));
-Router.get('/oidc/callback', passport.authenticate('openidconnect', { failWithError: true, failureMessage: true, session: false }), (req, res) => {
-  console.log('saransh', req);
-  const { accessToken, refreshToken } = req.user;
-  return res.redirect(`${process.env.WEBSITE_HOME}/login?accessToken=${accessToken}&refreshToken=${refreshToken}`);
-});
 /*
  * Router.get('/refresh-token/:token', refreshToken);
  */
