c4fad2d7f852a7ddefd3287462e5cf3c627fd216
Saransh Balyan <saransh.balyan@m2pfintech.com>
2024-12-21 20:33:59 +0530

    minor

--- User/Routes/passport.unprotected.routes.js
+++ User/Routes/passport.unprotected.routes.js
@@ -7,20 +7,22 @@ import { callbackStrategy, startStrategy } from '../Controllers/passport.control
 const Router = express.Router();
 
 const middleware = session({
-    cookie: {
-      httpOnly: true,
-      maxAge: 60 * 1000,
-      secure: false
-    },
-    resave: false,
-    saveUninitialized: false,
-    secret: 'saransh',
-//     store: MongoStore.create({
-//       mongoUrl: 'mongodb+srv://saransh:ysoserious@saransh.jvitvgq.mongodb.net/passport-session'
-//   })
-  })
+  cookie: {
+    httpOnly: true,
+    maxAge: 60 * 1000,
+    secure: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  secret: 'saransh'
+/*
+ *     store: MongoStore.create({
+ *       mongoUrl: 'mongodb+srv://saransh:ysoserious@saransh.jvitvgq.mongodb.net/passport-session'
+ *   })
+ */
+});
 
-Router.get('/:type/start', middleware,startStrategy);
-Router.get('/:type/callback', middleware,callbackStrategy);
+Router.get('/:type/start', middleware, startStrategy);
+Router.get('/:type/callback', middleware, callbackStrategy);
 
 export default Router;
