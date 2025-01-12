3d4bf36070067e89b2eacad246b3e3090200ba35
Saransh Balyan <saranshbalyan1234@gmail.com>
2025-01-13 00:28:56 +0530

    test

--- Notification/Whatsapp/whatsapp.service.js
+++ Notification/Whatsapp/whatsapp.service.js
@@ -1,14 +1,15 @@
 import pkg from 'whatsapp-web.js'
 import qrcode from 'qrcode-terminal'
+import {incomingConst, outgoingConst} from './gaali.constant.js'
 const { Client, LocalAuth } = pkg;
 
 // Create a new client instance
 const client = new Client({
     puppeteer:
     {
-        headless: true,
+        headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: '/usr/bin/google-chrome-stable'
+        // executablePath: '/usr/bin/google-chrome-stable'
     },
     authStrategy: new LocalAuth()
 });
@@ -24,11 +25,17 @@ client.on('qr', (qr) => {
 });
 
 client.on('message_create', (message) => {
-    console.log( message.body)
-    if (message.body === 'ping') {
-		// send back "pong" to the chat the message was sent in
-		message.reply('pong');
-	}
+    if (message.type == 'chat' && typeof message.body == "string") {
+        console.log(message.body)
+        if (message.body === 'ping') {
+            // send back "pong" to the chat the message was sent in
+            message.reply('pong');
+        }
+        if (incomingConst.includes(message.body.toLocaleLowerCase())) { 
+            const randomIndex = Math.floor(Math.random() * outgoingConst.length);
+            message.reply(outgoingConst[randomIndex]);
+        }
+    }
 })
 
 // Start your client
