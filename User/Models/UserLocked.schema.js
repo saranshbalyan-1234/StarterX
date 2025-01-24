31bfec52cdfdddd8eac55a40f796a1d37647155f
Saransh Balyan <saransh.balyan@m2pfintech.com>
2025-01-24 11:59:58 +0530

    minor

--- User/Models/UserLocked.schema.js
+++ User/Models/UserLocked.schema.js
@@ -2,12 +2,21 @@ import mongoose from 'mongoose';
 
 import BaseSchema from '#utils/Mongo/BaseSchema.js';
 
+const expireAfter = 10; //seconds
+
 const UserLocked = BaseSchema({
-  createdAt: { expires: 10, type: Date },
+  createdAt: { expires: expireAfter, type: Date },
   customer: {
     default: null,
     ref: 'user',
     type: mongoose.Schema.Types.ObjectId
+  },
+  unlockAt: {
+    default: function calculateUnlockAt () {
+      // Calculate unlockAt as createdAt + expireAfter
+      return new Date(Date.now() + expireAfter * 1000);
+    },
+    type: Date
   }
 });
 
