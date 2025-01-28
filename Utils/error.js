1dd0c1fb036e1825e9f174ddc822145e3c59a87f
Saransh Balyan <saransh.balyan@m2pfintech.com>
2025-01-29 01:54:04 +0530

    .

--- Utils/error.js
+++ Utils/error.js
@@ -1,7 +1,8 @@
 const getError = (e, res) => {
+  e.message = e.message ? e.message.replaceAll('Error: ', '').replaceAll('ApiError: ', '') : '';
   console.error(e);
   if (!res || res.headersSent) return;
-  return res.status(e.statusCode || 500).json({ error: e.message.replace('Error: ', ''), type: e.customName === 'ApiError' ? 'Error' : e.customName });
+  return res.status(e.statusCode || 500).json({ error: e.message, type: e.customName === 'ApiError' ? 'Error' : e.customName });
 };
 
 export default getError;
