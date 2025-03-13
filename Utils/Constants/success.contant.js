5cc3242446ad3fd21f55b415aff19b7c676c49b0
saransh <saranshbalyan1234@gmail.com>
2025-03-14 03:40:57 +0530

    /

--- Utils/Constants/success.contant.js
+++ Utils/Constants/success.contant.js
@@ -2,6 +2,7 @@ const successContstants = {
   DELETED: 'Deleted Succesfully!',
   EMAIL_VERIFICATION_SUCCESSFULL: 'Email Verified successfull',
   PASSWORD_RESET_SUCCESSFULL: 'Passwors Reset successfull',
-  UPDATED: 'Updated Succesfully!'
+  UPDATED: 'Updated Succesfully!',
+  FILE_UPLOADED: 'File uploaded successfully',
 };
 export default successContstants;
