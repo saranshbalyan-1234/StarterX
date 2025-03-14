3d11333a84d0733f19c1aff7e60a1a1bb01d88eb
Saransh Balyan <saransh.balyan@m2pfintech.com>
2025-03-15 02:39:05 +0530

    multer

--- Storage/multer.js
+++ Storage/multer.js
@@ -1,16 +1,40 @@
 import multer from 'multer';
 import path from 'path';
 
-// Multer Storage (Saves in /uploads folder)
+// Set up Multer Storage (Saves in /uploads folder)
 const storage = multer.diskStorage({
     destination: './uploads',
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
         const name = path.basename(file.originalname, ext);
-        cb(null, `${name}-${Date.now()}.${ext}`);
+        cb(null, `${name}-${Date.now()}${ext}`);
     }
 });
 
-const upload = multer({ storage: storage });
+// File Filter function
+const fileFilter = (req, file, cb) => {
+    // Accept images only (png, jpg, jpeg, gif)
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-export default upload
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: File upload only supports the following filetypes - ' + filetypes);
+    }
+};
+
+// Limits for the upload
+const limits = {
+    fileSize: 1 * 1024 * 1024 // 1 MB (change as needed)
+};
+
+// Set up the upload middleware
+const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: limits
+});
+
+export default upload;
