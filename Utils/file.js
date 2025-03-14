8b0dea0bb76ba17ddf80c2e014c0fb910a9b18d1
saransh <saranshbalyan1234@gmail.com>
2025-03-15 03:58:21 +0530

    fix

--- Utils/file.js
+++ Utils/file.js
@@ -1,8 +1,8 @@
-import glob from 'fast-glob';
-// needs to be removed and back to glob
-export const getDirectories = async (src = '.', filetype) => {
+import glob from 'glob';
+
+export const getDirectories = function getDirectories (src = '.', filetype) {
   try {
-    const files = await glob(`${src}/**/*.${filetype}.js`);
+    const files = glob(`${src}/**/*.${filetype}.js`, { sync: true });
     return files || [];
   } catch (err) {
     console.error(err);
